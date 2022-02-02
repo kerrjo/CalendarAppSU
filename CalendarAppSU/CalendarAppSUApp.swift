a2e4c34737175181c073f798485019486f8b33db
Joe <kerrjo@me.com>
2022-02-01 23:19:20 -0500

    Initial Commit

---
+++ CalendarAppSU/CalendarAppSUApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarAppSUApp.swift
+//  CalendarAppSU
+//
+//  Created by JOSEPH KERR on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct CalendarAppSUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalendarAppSU/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalendarAppSU
+//
+//  Created by JOSEPH KERR on 2/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
